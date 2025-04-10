34417d8dbe951aed5665e13f9db260cae781b7c5
labanidas <labanidas2001@gmail.com>
2025-04-10 17:59:18 +0530

    omeon solutions

---
+++ src/components/omron/OmronAutomationCenter.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const OmronAutomationCenter = () => {
+  return (
+    <div>
+      <h2>Omron Automation Center</h2>
+      <p>Explore our advanced automation solutions.</p>
+    </div>
+  )
+}
+
+export default OmronAutomationCenter
